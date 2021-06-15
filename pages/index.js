eacc7992a0328d1f3107ef8b5b64d3c8fd83634e
fuzailpolin <fuzail.polin2@gmail.com>
2021-06-16 02:02:02 +0600

    particle js added

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Layouts from "../Layouts"
+import Particles from 'react-particles-js';
+import particleConfig from '../config/particle-config';
+const img = '/images/warning.png';
 
 export default function Home() {
   return (
     <div>
       <Layouts>
-        index
+        <div style={{display:'flex', justifyContent:'center', alignItems:'center', height: '100vh'}}>
+          <div style={{backgroundColor: '#131010', position:'absolute', width: '100vw', height: '100vh' }}>
+            <Particles params={particleConfig} />
+          </div>
+          <div style={{position: 'relative', display:'flex', alignItems: 'center'}}>
+            <img style={{height: 80, width: 80}} src={img} alt='not found' />
+            <p style={{color: 'white', fontSize: 60}}>Under Construction</p>
+          </div>
+        </div>
       </Layouts>
     </div>
   )
